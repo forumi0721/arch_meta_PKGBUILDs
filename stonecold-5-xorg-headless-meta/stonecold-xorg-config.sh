468601fde9c25f13195f1a07de37f389b2de2acb
forumi0721 <forumi0721@gmail.com>
2015-10-27 01:25:04 +0900

    stonecold-5-xorg-headless-meta: Update

--- stonecold-5-xorg-headless-meta/stonecold-xorg-config.sh
+++ stonecold-5-xorg-headless-meta/stonecold-xorg-config.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
-sed -i "s/^#display-setup-script=/display-setup-script=\/etc\/lightdm\/display-setup-script.sh/g" /etc/lightdm/lightdm.conf 
-sed -i "s/^#xserver-command=X/xserver-command=X -s 0 dpms/g" /etc/lightdm/lightdm.conf
+if [ -e /etc/lightdm/lightdm.conf ]; then
+	sed -i "s/^#display-setup-script=/display-setup-script=\/etc\/lightdm\/display-setup-script.sh/g" /etc/lightdm/lightdm.conf 
+	sed -i "s/^#xserver-command=X/xserver-command=X -s 0 dpms/g" /etc/lightdm/lightdm.conf
+fi
 
 if [ -z "$(grep '<property name="shutdown" type="empty">' /etc/xdg/xfce4/xfconf/xfce-perchannel-xml/xfce4-session.xml)" ]; then
 	sed -i 's#^</channel>$#  <property name="shutdown" type="empty">\
