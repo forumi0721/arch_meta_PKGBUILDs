5f5040ef2abcb75d08d351e3a788f10f971f017e
forumi0721 <forumi0721@gmail.com>
2015-10-28 13:28:29 +0900

    stonecold-3-server-meta: Update

--- stonecold-3-server-meta/stonecold-server-config.sh
+++ stonecold-3-server-meta/stonecold-server-config.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 
 sed -i "s/^#disallow-other-stacks=no/disallow-other-stacks=yes/g" /etc/avahi/avahi-daemon.conf
+if [ -z "$(grep -w 'include /etc/nginx/conf.d/*..conf;' /etc/nginx/nginx.conf)" ]; then
+	echo 'include /etc/nginx/conf.d/*.conf;' >> /etc/nginx/nginx.conf
+fi
+if [ -z "$(grep -w 'include /etc/nginx/sites-enabled/\*;' /etc/nginx/nginx.conf)" ]; then
+	echo 'include /etc/nginx/sites-enabled/*;' >> /etc/nginx/nginx.conf
+fi
