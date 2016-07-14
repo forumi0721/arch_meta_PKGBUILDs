d07b2f9f6849cd19016e694b79d0010285f19b62
forumi0721 <forumi0721@gmail.com>
2016-07-15 00:01:39 +0900

    stonecold-2-base-devel-meta: Update

--- stonecold-2-base-devel-meta/stonecold-base-devel-config.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-if [ "${EUID}" != "0" ]; then
-	sudo "${0}" $@
-	exit $?
-fi
-
-sed -i "s/^DISTCC_ARGS=\"--allow 127.0.0.1\"/DISTCC_ARGS=\"--user nobody --allow 192.168.0.0\/24\"/g" /etc/conf.d/distccd
